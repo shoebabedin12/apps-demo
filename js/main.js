e01d87c8ebdafa71a933200db461c0cac5d8bae5
shoebabedin <shoeb.abedin@intelli.global>
2023-09-04 11:55:23 +0600

    update

--- js/main.js
+++ js/main.js
@@ -54,4 +54,49 @@ $(document).ready(function () {
     },
   });
 
+
+
+  // var swiper = new Swiper(".imageSlider", {
+  //   loop: true,
+  //   spaceBetween: 10,
+  //   slidesPerView: 1,
+  //   freeMode: true,
+  //   watchSlidesProgress: true,
+  // });
+  // var swiper2 = new Swiper(".text", {
+  //   loop: true,
+  //   spaceBetween: 10,
+  //   slidesPerView: 1,
+  //   navigation: {
+      
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   thumbs: {
+  //     swiper: swiper,
+  //   },
+  // });
+
 })
+
+
+var galleryTop = new Swiper('.imageSlider', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+   loop: true,
+  loopedSlides: 4
+});
+var galleryThumbs = new Swiper('.text', {
+  spaceBetween: 10,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 4
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
