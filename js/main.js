ba8c973bd36edf1ab0b14377f9f624e6c7b067a3
shoeb abedin <shoebabedin12@gmail.com>
2023-09-06 17:33:36 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  // Preloader
+  setTimeout(function() {
+    $('#container').addClass('loaded');
+    // Once the container has finished, the scroll appears
+    if ($('#container').hasClass('loaded')) {
+      // It is so that once the container is gone, the entire preloader section is deleted
+      $('#preloader').delay(9000).queue(function() {
+        $(this).remove();
+      });}
+  }, 3000);
   // sticky nav
   var btn = $(".navbar");
 
